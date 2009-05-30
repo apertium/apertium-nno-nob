952f2a73c8d43b113bf254761cf1b985575b8ecd
Francis M. Tyers <ftyers@prompsit.com>
2009-05-30 15:28:54 +0000

    fix

--- dev/inconsistency.sh
+++ dev/inconsistency.sh
@@ -1,6 +1,6 @@
 TMPDIR=/tmp
-lt-expand apertium-nn-nb.nb.dix | grep -v '<prn><enc>' | grep -e ':<:' -e '\w:\w' | sed 's/:<:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
+lt-expand ../apertium-nn-nb.nb.dix | grep -v '<prn><enc>' | grep -e ':<:' -e '\w:\w' | sed 's/:<:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
         apertium-pretransfer|
-        apertium-transfer apertium-nn-nb.nb-nn.t1x  nb-nn.t1x.bin  nb-nn.autobil.bin | tee $TMPDIR/tmp_testvoc2.txt | 
-        lt-proc -d nb-nn.autogen.bin > $TMPDIR/tmp_testvoc3.txt
+        apertium-transfer ../apertium-nn-nb.nb-nn.t1x  ../nb-nn.t1x.bin  ../nb-nn.autobil.bin | tee $TMPDIR/tmp_testvoc2.txt | 
+        lt-proc -d ../nb-nn.autogen.bin > $TMPDIR/tmp_testvoc3.txt
 paste -d _ $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc3.txt | sed 's/\^.<sent>\$//g' | sed 's/_/   --------->  /g'
