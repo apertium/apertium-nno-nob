017562ea9558261a21241772e1efb53be55ee531
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2010-09-19 17:39:07 +0000

    grep out compounds in non-cp testvoc, add nn_a.autogen to scripts; minor fixes

--- dev/nb-nn.inconsistency.sh
+++ dev/nb-nn.inconsistency.sh
@@ -6,7 +6,7 @@ SLANG=nb
 PREFIX=nb-nn
 BASENAME=apertium-nn-nb
 
-lt-expand ${DEV}/../${BASENAME}.${SLANG}.dix | grep -v '<prn><enc>\|DUE_TO_LT_PROC_HANG\|__REGEXP__' | grep -e ':>:' -e "[$ALPHABET]:[$ALPHABET]" | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent><clb>$/g' | tee $TMPDIR/tmp_${PREFIX}_testvoc1.txt |
+lt-expand ${DEV}/../${BASENAME}.${SLANG}.dix | grep -v '<prn><enc>\|DUE_TO_LT_PROC_HANG\|__REGEXP__\|<compound-only-L>\|<compound-R>' | grep -e ':>:' -e "[$ALPHABET]:[$ALPHABET]" | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent><clb>$/g' | tee $TMPDIR/tmp_${PREFIX}_testvoc1.txt |
         apertium-pretransfer|
         apertium-transfer ${DEV}/../${BASENAME}.${PREFIX}.t1x  ${DEV}/../${PREFIX}.t1x.bin  ${DEV}/../${PREFIX}.autobil.bin | tee $TMPDIR/tmp_${PREFIX}_testvoc2t.txt |
 	apertium-interchunk ${DEV}/../${BASENAME}.${PREFIX}.t2x  ${DEV}/../${PREFIX}.t2x.bin | tee $TMPDIR/tmp_${PREFIX}_testvoc2i.txt |
@@ -14,3 +14,9 @@ lt-expand ${DEV}/../${BASENAME}.${SLANG}.dix | grep -v '<prn><enc>\|DUE_TO_LT_PR
         lt-proc -d  ${DEV}/../${PREFIX}.autogen-no-cp.bin > $TMPDIR/tmp_${PREFIX}_testvoc3.txt
 
 paste -d _ $TMPDIR/tmp_${PREFIX}_testvoc1.txt $TMPDIR/tmp_${PREFIX}_testvoc2t.txt $TMPDIR/tmp_${PREFIX}_testvoc3.txt | sed 's/\^.<sent><clb>\$//g' | sed 's/_/   --------->  /g' | sed 's/\\//g'
+
+
+# nb->nn_a:
+lt-proc -d  ${DEV}/../${PREFIX}_a.autogen-no-cp.bin > $TMPDIR/tmp_${PREFIX}_testvoc3_a.txt
+
+paste -d _ $TMPDIR/tmp_${PREFIX}_testvoc1.txt $TMPDIR/tmp_${PREFIX}_testvoc2t.txt $TMPDIR/tmp_${PREFIX}_testvoc3_a.txt | sed 's/\^.<sent><clb>\$//g' | sed 's/_/   --------->  /g' | sed 's/\\//g'
