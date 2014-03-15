189bbb270e5b3c976c4f761fd8d5a3695e922606
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2014-03-15 17:39:41 +0000

    nno-nob depends on nno and nob, using lt-trim

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,13 @@
 #! /bin/sh
 
+# Append any prefix from PKG_CONFIG_PATH to ACLOCAL_PATH
+OLDIFS=IFS
+IFS=':'
+for dir in $PKG_CONFIG_PATH; do
+    acdir="$dir/../../share/aclocal"
+    [ -x "$acdir" ] && ACLOCAL_PATH="${ACLOCAL_PATH}:$acdir"
+done
+IFS=OLDIFS
+export ACLOCAL_PATH
+
 autoreconf -fi && ./configure "$@"
