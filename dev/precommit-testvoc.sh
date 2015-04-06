b1d7842ff95bb23aa6b797fb0b99a60cc1e868c4
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2015-04-06 12:30:11 +0000

    hovudveg→hovedvei

--- dev/precommit-testvoc.sh
+++ dev/precommit-testvoc.sh
@@ -12,6 +12,7 @@ if [[ -d "$(dirname "$0")"/../.git ]]; then
     vc=git
 fi
 
+cd "$(dirname $0)/.."
 
 make -j4 langs
 
@@ -19,14 +20,16 @@ echo "Translating changes from $vc diff:"
 
 declare -i e=0
 
+eval $(grep ^AP_SRC config.log)
+
+$vc diff $AP_SRC1/apertium-$lang1.$lang1.dix | awk -F'lm="|"' '$2{print $2}' | apertium -d . $lang1-$lang2 | grep '[#@/]' && (( e++ ))
+$vc diff $AP_SRC2/apertium-$lang2.$lang2.dix | awk -F'lm="|"' '$2{print $2}' | apertium -d . $lang2-$lang1 | grep '[#@/]' && (( e++ ))
 $vc diff $basename.$lang1-$lang2.dix | awk -F'<l>|</l>' '{gsub(/<s [^>]*>/,"")} $2{print $2}' | apertium -d . $lang1-$lang2 | grep '[#@/]' && (( e++ ))
 $vc diff $basename.$lang1-$lang2.dix | awk -F"<r>|</r>" '{gsub(/<s [^>]*>/,"")} $2{print $2}' | apertium -d . $lang2-$lang1 | grep '[#@/]' && (( e++ ))
-$vc diff $basename.$lang1.dix        | awk -F'lm="|"' '$2{print $2}'                          | apertium -d . $lang1-$lang2 | grep '[#@/]' && (( e++ ))
-$vc diff $basename.$lang2.dix        | awk -F'lm="|"' '$2{print $2}'                          | apertium -d . $lang2-$lang1 | grep '[#@/]' && (( e++ ))
 
 
 if [[ $e -eq 0 ]]; then
-    echo "Uncommitted changes to $basename.$lang1.dix, $basename.$lang2.dix and $basename.$lang1-$lang2.dix appear testvoc clean, go ahead and commit."
+    echo "Uncommitted changes to $lang1.dix, $lang2.dix and $lang1-$lang2.dix appear testvoc clean; go ahead and commit."
 else
     echo "Uh-oh, there appear to be inconsistencies in your uncommitted changes. Please fix before committing."
 fi
