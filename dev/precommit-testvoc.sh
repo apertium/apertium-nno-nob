548747419ce87575a88090d4277e65dcb5e76b6f
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2016-04-12 10:11:08 +0000

    precommit-testvoc
    (do a simple testvoc of the git/svn diff of your uncommitted changes)

--- dev/precommit-testvoc.sh
+++ dev/precommit-testvoc.sh
@@ -12,7 +12,7 @@ if [[ -d "$(dirname "$0")"/../.git ]]; then
     vc=git
 fi
 
-cd "$(dirname $0)/.."
+cd "$(dirname "$0")/.."
 
 make -j4 langs
 
@@ -22,8 +22,8 @@ declare -i e=0
 
 eval $(grep ^AP_SRC config.log)
 
-$vc diff $AP_SRC1/apertium-$lang1.$lang1.dix | awk -F'lm="|"' '$2{print $2}' | apertium -d . $lang1-$lang2-dgen | grep '[#@/]' && (( e++ ))
-$vc diff $AP_SRC2/apertium-$lang2.$lang2.dix | awk -F'lm="|"' '$2{print $2}' | apertium -d . $lang2-$lang1-dgen | grep '[#@/]' && (( e++ ))
+$vc diff "${AP_SRC1}"/apertium-$lang1.$lang1.dix | awk -F'lm="|"' '$2{print $2}' | apertium -d . $lang1-$lang2-dgen | grep '[#@/]' && (( e++ ))
+$vc diff "${AP_SRC2}"/apertium-$lang2.$lang2.dix | awk -F'lm="|"' '$2{print $2}' | apertium -d . $lang2-$lang1-dgen | grep '[#@/]' && (( e++ ))
 $vc diff $basename.$lang1-$lang2.dix | awk -F'<l>|</l>' '{gsub(/<s [^>]*>/,"")} $2{print $2}' | apertium -d . $lang1-$lang2-dgen | grep '[#@/]' && (( e++ ))
 $vc diff $basename.$lang1-$lang2.dix | awk -F"<r>|</r>" '{gsub(/<s [^>]*>/,"")} $2{print $2}' | apertium -d . $lang2-$lang1-dgen | grep '[#@/]' && (( e++ ))
 
