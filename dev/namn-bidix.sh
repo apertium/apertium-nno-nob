db9a51add82ac68d7f065fcd70e494c43f7d3a98
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2018-10-22 19:25:06 +0200

    namelists to xml-scripts

---
+++ dev/namn-bidix.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sed 's/.*/[&]&/'                                                                                       \
+    | apertium -f none -d . nob-nno_e-morph                                                            \
+    | grep -e '\^.*\^' -e '[*]'                                                                        \
+    | sed 's/].*//;s/\[//'                                                                             \
+    | awk '{i=$0;gsub(/ /,"<b/>",i);print "<e><p><l>"i"<s n=\"np\"/></l><r>"i"<s n=\"np\"/></r></e>"}' \
+    | rev                                                                                              \
+    | sort                                                                                             \
+    | rev
