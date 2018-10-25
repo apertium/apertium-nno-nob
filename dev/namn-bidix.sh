e1ba3ead6d2edd53acc1a2802317899ae8333f5a
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2018-10-25 14:45:58 +0200

    Stortingets administrasjon→stortingsadministrasjonen

--- dev/namn-bidix.sh
+++ dev/namn-bidix.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sed 's/.*/[&]&/'                                                                                       \
-    | apertium -f none -d . nob-nno_e-morph                                                            \
-    | grep -e '\^.*\^' -e '[*]'                                                                        \
-    | sed 's/].*//;s/\[//'                                                                             \
-    | awk '{i=$0;gsub(/ /,"<b/>",i);print "<e><p><l>"i"<s n=\"np\"/></l><r>"i"<s n=\"np\"/></r></e>"}' \
-    | rev                                                                                              \
-    | sort                                                                                             \
+sed 's/.*/[&]&/'                                                                                           \
+    | apertium -f none -d . nob-nno_e-morph                                                                \
+    | grep -e '\^.*\^' -e '[*]'                                                                            \
+    | sed 's/].*//;s/\[//'                                                                                 \
+    | awk '{i=$0;gsub(/ /,"<b/>",i);print "<e><p><l>"i"<s n=\"np\"/></l><r>"i"<s n=\"np\"/></r></p></e>"}' \
+    | rev                                                                                                  \
+    | sort                                                                                                 \
     | rev
