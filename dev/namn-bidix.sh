c4f4f63e0d2c8470f504f8ace103ea920ae34dfb
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2024-04-11 15:43:11 +0200

    dev/namn-bidix.sh: correctly grep out known np's

--- dev/namn-bidix.sh
+++ dev/namn-bidix.sh
@@ -2,7 +2,7 @@
 
 sed 's/.*/[&]&/'                                                                                           \
     | apertium -f none -d . nob-nno_e-morph                                                                \
-    | grep -e '\^.*\^' -e '[*]'                                                                            \
+    | grep -v '^\[\(.*\)].*/\1<np>'                                                                        \
     | sed 's/].*//;s/\[//'                                                                                 \
     | awk '{i=$0;gsub(/ /,"<b/>",i);print "<e><p><l>"i"<s n=\"np\"/></l><r>"i"<s n=\"np\"/></r></p></e>"}' \
     | rev                                                                                                  \
